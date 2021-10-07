1c7a468f777ce875a501cf19eb78572cb2cddb76
barmaiya <dev007mail@gmail.com>
2021-10-07 17:09:23 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,8 @@
 #setup provider which is aws in this case with aws credential
 provider "aws"{
     region="ap-south-1"
+    access_key = "AKIAXHUQFAXYFA4ZE6FD"
+    secret_key = "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"
 }
 
 #create ec2 instance
