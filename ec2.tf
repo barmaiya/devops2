344796acd898fc727549c4b3a9d9838dcf6b232c
barmaiya <dev007mail@gmail.com>
2021-10-07 17:04:52 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+#setup provider which is aws in this case with aws credential
+provider "aws"{
+    region="ap-south-1"
+}
+
+#create ec2 instance
+resource "aws_instance" "Node1"{
+    ami =""
+    type="t2.micro"
+    region  = "ap-south-1"
+}
+
+#create ec2 instance
+resource "aws_instance" "Node2"{
+    ami =""
+    type="t2.micro"
+    region  = "ap-south-1"
+}
+
+#create ec2 instance
+resource "aws_instance" "Node3"{
+    ami =""
+    type="t2.micro"
+    region  = "ap-south-1"
+}
